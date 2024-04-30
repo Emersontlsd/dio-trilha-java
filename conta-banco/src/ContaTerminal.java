85f8f34f6786bed04d20f01d8b86e6e26be96065
Emersontlsd <emersonsdom13@hotmail.com>
2024-04-30 00:39:49 -0300

    codigo modificado e funcionando

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        // Obter pela Scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+        Scanner dados = new Scanner(System.in);
+
+            int numeroConta = 1021;
+            String agencia = "067-8";
+            String nomeCliente = "";
+            double saldo = 1000.00;
+            
+
+            System.out.println("Vamos cadastrar sua conta.");
+
+            System.out.println("");
+            
+            System.out.println("Informe o número da conta:");
+            numeroConta = dados.nextInt();
+            dados.nextLine();
+
+            System.out.println("");
+
+            System.out.println("Informe o número da agência com o - (hífen):");
+            agencia = dados.nextLine();
+
+            System.out.println("");
+            
+            System.out.println("Informe seu nome:");
+            nomeCliente = dados.nextLine();
+
+            System.out.println("");
+
+            System.out.println("Informe o saldo da conta:");
+            String saldoStr = dados.nextLine();
+            saldo = Double.parseDouble(saldoStr);
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+   
+
     }
 }
